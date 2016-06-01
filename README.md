a9a4676a7354d4d840dbfc4bacef7f7d84d2d595
Dave Long <dlong@cagedata.com>
2016-06-01 14:22:31 -0400

    Add ansible-dk

---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Formulae
+
+A [Homebrew
+tap](https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/brew-tap.md)
+for various Cage Data projects and tools.
+
+## Usage
+
---
+++ Taps/ansible-dk.rb
@@ -0,0 +1,16 @@
+cask 'ansible-dk' do
+  version '1.1.2'
+  sha256 '0aabae1ee97d569367015b03f0f22d9ef12cac0912135726a478c2ab9f1c231d'
+
+  url "https://github.com/omniti-labs/ansible-dk/releases/download/#{version}/ansible-dk-#{version}-1.dmg"
+  name 'Ansible DK'
+  homepage 'https://github.com/omniti-labs/ansible-dk'
+  license :gratis
+
+  pkg "ansible-dk-#{version}-1.pkg"
+
+  zap delete: [
+    '/opt/ansible-dk',
+    '/usr/local/bin/ansible-dk'
+  ]
+end
